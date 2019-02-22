82f438f4cf4a329f30ea0c6422cae017cacabf79
johan cala <johan.cala.2019@upb.edu.co>
2019-02-22 18:48:59 -0500

    tercer commind

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-a=int(input("digite el numero que desee"))
-c=int(input("digite otro numero de interes:V"))
-DobleA=(2*a)
-producto=(a*c)
-print(DobleA)
-print(producto)
+import math
+
+a=int(input("ingrese termino a:"))
+b=int(input("ingrese termino b:"))
+c=int(input("ingrese termnio c:"))
+
+d=(b*b-4*a*c)
+
+if a!=0:
+    if d<0:
+        print("las raices son imaginarias...")
+    else:
+        x1=(-b+(math.sqrt(d)))/(2*a)
+        x2=(-b-(math.sqrt(d)))/(2*a)
+        print("X1 = "+str(x1)+" X2 = "+str(x2))
+else:
+    print("coefiente a debe ser diferente de cero")
