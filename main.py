7510baa50d4d8ebf2e49224c40a4feeac9764af0
johan cala <johan.cala.2019@upb.edu.co>
2019-02-22 19:24:20 -0500

    cuarto commit

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
+#este es el del 9 solo que se me olvido ponerlo en orden
 import math
 
-a=int(input("ingrese termino a:"))
-b=int(input("ingrese termino b:"))
-c=int(input("ingrese termnio c:"))
 
-d=(b*b-4*a*c)
+b=int(input("ingrese numero b:"))
 
-if a!=0:
-    if d<0:
-        print("las raices son imaginarias...")
-    else:
-        x1=(-b+(math.sqrt(d)))/(2*a)
-        x2=(-b-(math.sqrt(d)))/(2*a)
-        print("X1 = "+str(x1)+" X2 = "+str(x2))
-else:
-    print("coefiente a debe ser diferente de cero")
+
+d=int(input("ingrese numero d:"))
+
+print("el cuadrado del numero b es: " , (b*b))
+print("la raiz del numero d es: " , math.sqrt(d))
